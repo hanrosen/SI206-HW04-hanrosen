23e7289bc7b5f7fd23f4c502ba80ce09b12cf115
Hannah Rosenbaum <hanrosen@umich.edu>
2018-02-01 13:10:36 -0500

    A2

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,10 @@
 def question():
     q = input('What is your question? ')
+    while q != 'quit':
+        if "?" in q:
+            print('response')
+        else:
+            print("I'm sorry, I can only answer questions")
+        q = input('What is your question? ')
+
+question()
